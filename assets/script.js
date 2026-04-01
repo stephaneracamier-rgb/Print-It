fe622caeff83543de4c670e24b819a65bc7bb791
Stéphane Racamier <stephane.racamier@gmail.com>
2026-04-01 16:36:33 +0200

    docs(js) : fichier script.js commenté avec les éléments de code intégrés

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,12 @@
+/*Script ayant pour objectif :
+Lors d'un clic droit le passage au visuel suivant sur la droite avec coloration du Bullet Point correpondant
+Lors d'un clic à gauche le passage au visuel suivant sur la gauche avec coloration du Bullet Point correpondant
+Lors de l'arrivée au dernier ou au premier visuel défilement continu pour revenir au point de départ
+Avec cela insertion d'une Tagline sur tous les visuels*/
 
-/*const bulletPoint = document.querySelector(".dots");
-bulletPoint.classList.add("dot")
-console.log(bulletPoint)*/
 
-/*const bulletPointSelected = document.querySelector(".dots");
-bulletPointSelected.classList.add("dot_selected")
-console.log(bulletPointSelected)*/
 
+/*Ecoute des événements*/
 let clicGauche = document.querySelector(".arrow_left");
 clicGauche.addEventListener("click", function () {
         console.log("Vous avez cliqué sur le chevron gauche")
@@ -16,22 +16,21 @@ let clicDroit = document.querySelector(".arrow_right");
 clicDroit.addEventListener("click", function () {
         console.log("Vous avez cliqué sur le chevron droit")
     });
-
+    
+/*Tableau des visuels du slider et comptage*/
 let bannerImg = ["slide1", "slide2", "slide3", "slide4"]
 for (let i = 0; i < 4; i++) {
 console.log(i)
 }
 
-let contenuTitre = "Impressions tous formats"
-let contenuParagraphe = "en boutique et en ligne"
+/*Insertion de la Tagline dans le slider*/
+let tagline = "Impressions tous formats <span>en boutique et en ligne</span>"
+
+let P =
+        <p>${tagline}</p>   
+    ;
 
 let banner = document.querySelector("banner")
+banner.innerHTML = P   
 
-let HTML =
-    <div id="banner">
-        <p>${contenuTitre}</p>
-        <span>${contenuTitre}</span>
-    </div>
-
-banner.innerHTML
 

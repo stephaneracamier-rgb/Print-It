a2c08ff8af90c6fcc3744ef5f66b148d3a0e55a4
Stéphane Racamier <stephane.racamier@gmail.com>
2026-04-23 08:54:54 +0200

    feat(main) 1ere version du script du Slide

--- assets/script.js
+++ assets/script.js
@@ -5,8 +5,13 @@ Lors de l'arrivée au dernier ou au premier visuel défilement continu pour reve
 Avec cela insertion d'une Tagline sur tous les visuels*/
 
 
+EN PREMIER METTRE UN EVENT SUR CHEVRON DROITE ET UN EVENT SUR GAUCHE
+IMAGE APRES CLIC DEVIENT LA PRINCIPALE SUCCESSIVEMENT AVEC UNE CONDITION QUI RENVOIE A PREMIERE IMAGEEN FIN DE BOUCLE
+DOUBLE BOUCLE DANS UN SENS PUIS LAUTRE
 
-/*Ecoute des événements*/
+
+/*Ecoute des événements
+ABSENCE DE FONCTION ENTRAINANT LE PASSAGE AU VISUEL SUIVANT ET LE REMPLISSAGE DU BULLET POINT SUIVANT */
 let clicGauche = document.querySelector(".arrow_left");
 clicGauche.addEventListener("click", function () {
         console.log("Vous avez cliqué sur le chevron gauche")
@@ -23,7 +28,8 @@ for (let i = 0; i < 4; i++) {
 console.log(i)
 }
 
-/*Insertion de la Tagline dans le slider*/
+/*Insertion de la Tagline dans le slider
+PAS DE RECUPERATION DE L'ELEMENT A EFFECTUER */
 let tagline = "Impressions tous formats <span>en boutique et en ligne</span>"
 
 let P =
